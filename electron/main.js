589aec73a4e84fc6068f0aa5ab8f4a59229f2020
andy840119 <andy840119@gmail.com>
2025-01-28 15:40:08 +0800

    create the desktop environment. see: https://rbfraphael.medium.com/building-desktop-apps-with-electron-next-js-without-nextron-01bbf1fdd72e

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const appServe = null;
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+
+    if (app.isPackaged) {
+        appServe(win).then(() => {
+            win.loadURL("app://-");
+        });
+    } else {
+        win.loadURL("http://localhost:3000");
+        win.webContents.openDevTools();
+        win.webContents.on("did-fail-load", (e, code, desc) => {
+            win.webContents.reloadIgnoringCache();
+        });
+    }
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin"){
+        app.quit();
+    }
+});
