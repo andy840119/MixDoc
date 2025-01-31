cd0ec130685b0f133a4340d15c4326155adbde69
andy840119 <andy840119@gmail.com>
2025-01-31 16:33:27 +0800

    Define the path and directory object.

---
+++ src/workspace/types/path.ts
@@ -0,0 +1,41 @@
+export type Directory = string;
+
+function validateDirectory(name: Directory): boolean {
+  const trimmed = name.trim();
+  return trimmed.length !== 0;
+}
+
+export class Path {
+  private readonly _directories: Directory[];
+
+  constructor(input: string | Directory[]) {
+    if (Array.isArray(input)) {
+      this._directories = input;
+    } else {
+      this._directories = input.split('/');
+    }
+
+    // validate the directory is valid.
+    this._directories.forEach((x) => {
+      if (!validateDirectory(x)) {
+        throw new Error(`Invalid directory ${x}`);
+      }
+    });
+  }
+
+  get fullPath(): string {
+    return this._directories.join('/');
+  }
+
+  get directories(): Directory[] {
+    return [...this._directories];
+  }
+
+  append(directoryName: string): Path {
+    return new Path([...this._directories, directoryName]);
+  }
+
+  toString(): string {
+    return this.fullPath;
+  }
+}
